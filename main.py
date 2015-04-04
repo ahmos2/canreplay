58bb7c0320b741e91a5af3b04dbf0babebbd4b78
Martin Sundhaug <martinsundhaug@gmail.com>
2015-04-05 01:40:31 +0200

    Delay-correction 1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ sock = libcanopen.can_socket_open_timeout('vcan0', 0)
 
 inp=pcap.pcap("../capture/can0-1424041603.pcap")
 
-x,prevTS=0,0
+x,prevTS,skew,toSleep,preSleep=0,0,0,0,0
 for ts,pkt in inp:
     buf=(c_uint8*16)()
     for i in range(0,4): #Copy header (it's somehow backwards)
@@ -19,9 +19,18 @@ for ts,pkt in inp:
     if x % 1000 == 0:
         print x
 
-    if prevTS <> 0:
-        sleep(ts-prevTS)
+    if prevTS > 0:
+        toSleep=ts-prevTS
     prevTS=ts
+
+    if skew > 0:
+        toSleep-=skew
+
+    if toSleep > 0:
+        preSleep=time()
+        sleep(toSleep)
+        postSleep=time()
+        skew=postSleep-preSleep-toSleep
 
     libc.write(sock,byref(buf),c_int(16))
 
